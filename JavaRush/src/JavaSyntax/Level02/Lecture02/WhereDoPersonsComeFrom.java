105ef58a5421ca399078ce16e635c7f82895f79a
EvgeniyBuzmakov <EvgeniyBuzmakov86@gmail.com>
2022-04-13 02:35:33 +0500

    Level02, Lecture02, task WhereDoPersonsComeFrom

---
+++ JavaRush/src/JavaSyntax/Level02/Lecture02/WhereDoPersonsComeFrom.java
@@ -0,0 +1,20 @@
+package JavaSyntax.Level02.Lecture02;
+/*
+Откуда берутся Person?
+В классе Person объяви следующие переменные: name типа String, age типа int, weight типа int, money типа int.
+В методе main создай объект Person, занеси его ссылку в переменную person.
+*/
+public class WhereDoPersonsComeFrom {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Person person = new Person();
+    }
+
+    public static class Person {
+        //напишите тут ваш код
+        String name;
+        int age;
+        int weight;
+        int money;
+    }
+}
