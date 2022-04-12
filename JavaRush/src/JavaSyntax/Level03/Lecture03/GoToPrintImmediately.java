43d533bf7d4bf4512a3d8588ebe4be0b1a48ee1f
EvgeniyBuzmakov <evgeniybuzmakov86@gmail.com>
2022-04-13 02:04:10 +0500

    Level03, Lecture03, task GoToPrintImmediately

---
+++ JavaRush/src/JavaSyntax/Level03/Lecture03/GoToPrintImmediately.java
@@ -0,0 +1,13 @@
+package JavaSyntax.Level03.Lecture03;
+/*
+Немедленно в печать
+*/
+public class GoToPrintImmediately {
+    // напишите тут ваш код
+    public static void printString(String s) {
+        System.out.println(s);
+    }
+    public static void main(String[] args) {
+        printString("Hello, Amigo!");
+    }
+}
